04a110739d1914150070f66efe62d7fdc63a0292
Shienmy <nbamashuxiang@qq.com>
2023-05-03 08:04:27 +0800

    Site updated: 2023-05-03 08:04:27

--- js/main.js
+++ js/main.js
@@ -1,44 +1,74 @@
-const app = Vue.createApp({
-    mixins: Object.values(mixins),
-    data() {
-        return {
-            loading: true,
-            hiddenMenu: false,
-            showMenuItems: false,
-            menuColor: false,
-            scrollTop: 0,
-            renderers: [],
-        };
-    },
-    created() {
-        window.addEventListener("load", () => {
-            this.loading = false;
-        });
-    },
-    mounted() {
-        window.addEventListener("scroll", this.handleScroll, true);
-        this.render();
-    },
-    methods: {
-        render() {
-            if (typeof this.renderers === "undefined") return;
-            for (let i of this.renderers) i();
-        },
-        handleScroll() {
-            let wrap = this.$refs.homePostsWrap,
-                newScrollTop = document.documentElement.scrollTop;
-            if (this.scrollTop < newScrollTop) {
-                this.hiddenMenu = true;
-                this.showMenuItems = false;
-            } else this.hiddenMenu = false;
-            if (wrap) {
-                if (newScrollTop <= window.innerHeight - 100) this.menuColor = true;
-                else this.menuColor = false;
-                if (newScrollTop <= 400) wrap.style.marginTop = -newScrollTop / 5 + "px";
-                else wrap.style.marginTop = "-80px";
-            }
-            this.scrollTop = newScrollTop;
-        },
-    },
-});
-app.mount("#layout");
+/* global KEEP */
+
+window.addEventListener('DOMContentLoaded', () => {
+  const { version, local_search, code_block, code_copy, lazyload } = KEEP.theme_config
+
+  KEEP.themeInfo = {
+    theme: `Keep v${version}`,
+    author: 'XPoet',
+    repository: 'https://github.com/XPoet/hexo-theme-keep'
+  }
+
+  KEEP.localStorageKey = 'KEEP-THEME-STATUS'
+
+  KEEP.styleStatus = {
+    isExpandPageWidth: false,
+    isDark: false,
+    fontSizeLevel: 0,
+    isShowToc: true
+  }
+
+  // print theme base info
+  KEEP.printThemeInfo = () => {
+    console.log(
+      `\n %c ${KEEP.themeInfo.theme} %c ${KEEP.themeInfo.repository} \n`,
+      `color: #fadfa3; background: #333; padding: 6px 0;`,
+      `padding: 6px 0;`
+    )
+  }
+
+  // set styleStatus to localStorage
+  KEEP.setStyleStatus = () => {
+    localStorage.setItem(KEEP.localStorageKey, JSON.stringify(KEEP.styleStatus))
+  }
+
+  // get styleStatus from localStorage
+  KEEP.getStyleStatus = () => {
+    let temp = localStorage.getItem(KEEP.localStorageKey)
+    if (temp) {
+      temp = JSON.parse(temp)
+      for (let key in KEEP.styleStatus) {
+        KEEP.styleStatus[key] = temp[key]
+      }
+      return temp
+    } else {
+      return null
+    }
+  }
+
+  KEEP.refresh = () => {
+    KEEP.initUtils()
+    KEEP.initHeaderShrink()
+    KEEP.initModeToggle()
+    KEEP.initBack2Top()
+
+    if (local_search?.enable === true) {
+      KEEP.initLocalSearch()
+    }
+
+    if (
+      code_block?.tools?.enable === true ||
+      code_block?.enable === true ||
+      code_copy?.enable === true
+    ) {
+      KEEP.initCodeBlockTools()
+    }
+
+    if (lazyload?.enable === true) {
+      KEEP.initLazyLoad()
+    }
+  }
+
+  KEEP.printThemeInfo()
+  KEEP.refresh()
+})
